8f9edf25441e6e11fa1c6932bae25896e98823e4
Nesh98 <113505807+Nesh98@users.noreply.github.com>
2022-11-06 20:29:50 +0100

    Add files via upload

---
+++ JS Basics 11 ex1/script.js
@@ -0,0 +1,10 @@
+console.log('hello!')
+
+
+const secondCard = document.querySelector('.second-card');
+
+const secondCat = document.createElement('img');
+
+secondCat.src = "https://placekitten.com/408/287";
+
+secondCard.appendChild(secondCat);
