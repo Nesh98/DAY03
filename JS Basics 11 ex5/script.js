835f531252798a1d08f3a799a96a21326002150c
Nesh98 <113505807+Nesh98@users.noreply.github.com>
2022-11-06 20:39:05 +0100

    Add files via upload

---
+++ JS Basics 11 ex5/script.js
@@ -0,0 +1,12 @@
+console.log('hello!')
+
+
+const thirdBox = document.createElement("div");
+
+thirdBox.classList.add("box");
+
+
+const containerBox = document.querySelector(".container-boxes");
+
+
+containerBox.appendChild(thirdBox);
